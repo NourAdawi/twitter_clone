cfc5491a0efcac29c7fa3bfa6030c3d10802319d
NourAdawi <nouradwi12@gmail.com>
2021-05-02 10:33:10 +0300

    Add some changes!

--- script.js
+++ script.js
@@ -3,23 +3,36 @@ const tweetBtn = document.querySelector('.tweet-btn');
 const changeOpacity = x => {
   tweetBtn.style.opacity = x;
   
+
 };
+
 const textField = document.getElementById('text_field');
+
 textField.addEventListener('keypress', e => {
-  if(e.target.value !== ""){
+  if (e.target.value !== "") {
     changeOpacity(1);
   }
-  
-});
+   });
+
+
 
 tweetBtn.addEventListener('click', e => {
-  
+ 
+  var test = document.getElementById('text_field');
+      var tag = document.createElement("p");
+      var text = document.createTextNode(test.value);
+      tag.appendChild(text);
+      var element = document.getElementById("new");
+      element.appendChild(tag);
+
 });
 
-textField.addEventListener('blur', e => {
-  if(e.target.value === ""){
-    changeOpacity(0.5);
-  }
-  
-});
+
+
+  textField.addEventListener('blur', e => {
+    if (e.target.value === "") {
+      changeOpacity(0.5);
+    }
+
+  });
 
