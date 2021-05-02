6f3c212352535816f8e2be894d13ea402106b772
NourAdawi <nouradwi12@gmail.com>
2021-05-02 03:18:21 +0300

    no message

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+const tweetBtn = document.querySelector('.tweet-btn');
 
+const changeOpacity = x => {
+  tweetBtn.style.opacity = x;
+  
+};
+const textField = document.getElementById('text_field');
+textField.addEventListener('keypress', e => {
+  if(e.target.value !== ""){
+    changeOpacity(1);
+  }
+  
+});
 
-  const btn = document.querySelectorAll('#like');
-  btn.addEventListener('click', () => {btn.style.color='red'; });
+tweetBtn.addEventListener('click', e => {
+  
+});
+
+textField.addEventListener('blur', e => {
+  if(e.target.value === ""){
+    changeOpacity(0.5);
+  }
+  
+});
 
